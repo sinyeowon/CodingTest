e725c385b4f3285198896e4477c1bf70a53e7e8c
신여원 <140633914+sinyeowon@users.noreply.github.com>
2025-09-27 23:27:27 +0900

    [level 1] Title: 음양 더하기, Time: 0.07 ms, Memory: 97.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        
+        int length = absolutes.length;
+        
+        for(int i = 0; i<length; i++) {
+            if(signs[i] == true) {
+                answer += absolutes[i];
+            }
+            else {
+                answer -= absolutes[i];
+            }
+        }
+        
+        return answer;
+    }
+}
