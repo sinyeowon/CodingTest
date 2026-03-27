a8958b4ae5a95ba2ba79caa1a9d7186d48e9a7ce
신여원 <140633914+sinyeowon@users.noreply.github.com>
2026-03-28 03:07:35 +0900

    [Silver IV] Title: 스택 2, Time: 776 ms, Memory: 88788 KB -BaekjoonHub

---
+++ 백준/Python/Silver/28278. 스택 2/스택 2.py
@@ -0,0 +1,27 @@
+import sys
+
+n = int(input())
+stack = []
+
+for _ in range(n):
+    command = sys.stdin.readline().split()
+    
+    if command[0] == '1':
+        stack.append(command[1]);
+    elif command[0] == '2':
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
+    elif command[0] == '3':
+        print(len(stack))
+    elif command[0] == '4':
+        if stack:
+            print(0)
+        else:
+            print(1)
+    else:
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
