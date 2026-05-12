dbc5ab1236636d8a7501377a1a8d06f9db98a67b
신여원 <140633914+sinyeowon@users.noreply.github.com>
2026-05-12 12:04:04 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.04 ms, Memory: 62.2 MB -BaekjoonHub

---
+++ 프로그래머스/Java/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        
+        int target = Integer.bitCount(n);
+        
+        while(target != Integer.bitCount(++n)) {
+            
+        }
+        
+        return n;
+    }
+
+}
