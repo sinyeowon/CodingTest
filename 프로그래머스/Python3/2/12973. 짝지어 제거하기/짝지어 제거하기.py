45281ba8f75f415b48f85cf702a15e68c0ba9d15
신여원 <140633914+sinyeowon@users.noreply.github.com>
2026-05-14 14:45:51 +0900

    [level 2] Title: 짝지어 제거하기, Time: 61.93 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/Python3/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,15 @@
+def solution(s):
+
+    stack = []
+
+    for ch in s:
+
+        if stack and stack[-1] == ch:
+
+            stack.pop()
+
+        else:
+
+            stack.append(ch)
+
+    return 1 if not stack else 0
